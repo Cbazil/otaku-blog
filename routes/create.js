d4174608966b4195ab477cac8fbc370fdbd10b8e
Danielmhawes <chadbosch08@gmail.com>
2019-02-18 16:21:51 +0200

    working with summernotes to post description to db

--- routes/create.js
+++ routes/create.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Pokemon = require('../db.json');
+var Posts = require('../db.json');
 var request = require("request");
 
 /* GET create page. */
@@ -14,16 +14,20 @@ router.post('/', function(req, res, next) {
     // res.send(req.body);
 
     //set a new dynamic id
-    var id = Pokemon.pokemon.lenght;
+    var id = Posts.posts.lenght;
 
     //make a post request to our database
     request({
-    uri: "http://localhost:8000/pokemon",
+    uri: "http://localhost:8000/posts",
     method: "POST",
     form: {
         id: id,
         name: req.body.name,
+        aniName: req.body.aniName,
+        genre: req.body.genre,
+        author: req.body.author,
         image: req.body.image_url,
+        description: req.body.description,
     }
     }, function(error, response, body) {
         // console.log(body);
